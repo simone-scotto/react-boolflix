c986c7d7d35f0b5ce11edfc99491fd5589ea2390
simone-scotto <simone.scottotkd@gmail.com>
2025-07-29 17:17:40 +0200

    flaga added with an npm library but don't like, need to find another library

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { countries } from 'country-flag-icons'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
 /* import Header from "./components/Header";
 import Main from "./components/Main";
  */
 function App() {
-  const [movies, setMovies] = useState(null);
+  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
 
   const key = import.meta.env.VITE_API_KEY;
 
   const urlMovies = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`;
 
-  function handleClickMovies() {
+  function handleClickMovies(e) {
+    e.preventDefault()
+    
+    
     fetch(urlMovies)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setMovies(data);
-      });
+        console.log(data.results);  
+        const filteredMovies = data.results
+        setMovies(filteredMovies);
+        console.log(filteredMovies);
+              
+      }); 
   }
   return (
     <>
@@ -27,24 +35,24 @@ function App() {
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
               <img
-                src="../../public/Logo2.png"
+                src="/Logo2.png"
                 alt=""
                 width="30"
                 height="24"
                 className="d-inline-block align-text-top logo"
               />
             </a>
-            <form className="d-flex" role="search">
+            <form onSubmit={handleClickMovies} className="d-flex" role="search">
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
                 value={query}
-                onChange={(e) => setQuery(e.target.value)}
+                onChange={e => setQuery(e.target.value)}
               />
               <button
-                onClick={handleClickMovies}
+                
                 className="btn btn-outline-success"
                 type="submit"
               >
@@ -57,7 +65,27 @@ function App() {
       <main>
         <div className="container">
           <h2>Risultati</h2>
-          <ul>{movies.map(movie)}</ul>
+          <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+          
+            {movies.map((movie) => {
+              
+              return (
+                <div key={movie.id} className="col">
+                  <ul>
+                    <li>{movie.title}</li>
+                    <li>{movie.original_title}</li>
+                    <li>{movie.original_language}
+                      
+                      <img className="flag" src={`https://unpkg.com/language-icons/icons/${movie.original_language}.svg`} alt="" />
+                    
+                    </li>
+                    <li>{movie.vote_average}</li>
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
+          
         </div>
       </main>
     </>
